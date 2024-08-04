d45c81b476f7f9ca5936efe4841149c8a2f598f7
hdev1004 <h.dev@kakao.com>
2024-08-04 19:48:59 +0900

    add: 초기커밋

---
+++ ibo/src/main/java/com/erp/ibo/TestAPI.java
@@ -0,0 +1,13 @@
+package com.erp.ibo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestAPI {
+	
+	@GetMapping("/hello")
+	public String getHello() {
+		return "Hello IBO Project!";
+	}
+}
