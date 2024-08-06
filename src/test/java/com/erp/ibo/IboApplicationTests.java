4cae3c6d4d512fba74635e33ca5daa6469566dba
hdev1004 <h.dev@kakao.com>
2024-08-07 01:33:13 +0900

    fix: iboTest 파일 수정 - Github Actions 빌드 오류

--- src/test/java/com/erp/ibo/IboApplicationTests.java
+++ src/test/java/com/erp/ibo/IboApplicationTests.java
@@ -3,7 +3,7 @@ package com.erp.ibo;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class IboApplicationTests {
 
 	@Test
